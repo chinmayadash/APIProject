85b7e3502784691c76a725dd6acd447f787b2c4b
chinmayadash <chinmayadash.net@gmail.com>
2022-12-28 12:48:32 +0530

    sAS

--- APIProject/Controllers/WeatherForecastController.cs
+++ APIProject/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace APIProject.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "asc", "asc", "asc", "asc", "asc", "asc", "asc", "asc", "asc", "asc"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
